6031fc58e5b6f468685e887d21ae6f12834bfefc
ArielInguillay <arieling123@gmail.com>
2025-07-06 15:27:07 -0500

    update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PORT = int(os.getenv("PORT", 8082))
-DATABASE_URL = os.getenv("DATABASE_URL")
-JWT_SECRET = os.getenv("JWT_SECRET")
-JWT_ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
+def get_env_var(key: str, default=None, required=False):
+    value = os.getenv(key, default)
+    if required and value is None:
+        raise ValueError(f"❌ Variable de entorno obligatoria no definida: {key}")
+    return value
+
+PORT = int(get_env_var("PORT", 8082))
+DATABASE_URL = get_env_var("DATABASE_URL", required=True)
+JWT_SECRET = get_env_var("JWT_SECRET", required=True)
+JWT_ALGORITHM = get_env_var("JWT_ALGORITHM", "HS256")
